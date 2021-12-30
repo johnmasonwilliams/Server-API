5ebdd08baee4948b8a5854ba2adcfcb878b50c7a
Mason Williams <mwilliams@vantagetechnologies.io>
2021-12-29 19:41:41 -0500

    Commented out JWT until needed

--- src/api.ts
+++ src/api.ts
@@ -16,27 +16,27 @@ app.use(
   })
 );
 
-// Decodes the Firebase JSON Web Token
-app.use(decodeJWT);
+// // Decodes the Firebase JSON Web Token
+// app.use(decodeJWT);
 
-/**
- * Decodes the JSON Web Token sent via the frontend app
- * Makes the currentUser (firebase) data available on the body.
- */
- async function decodeJWT(req: Request, res: Response, next: NextFunction) {
-  if (req.headers?.authorization?.startsWith('Bearer ')) {
-    const idToken = req.headers.authorization.split('Bearer ')[1];
+// /**
+//  * Decodes the JSON Web Token sent via the frontend app
+//  * Makes the currentUser (firebase) data available on the body.
+//  */
+// async function decodeJWT(req: Request, res: Response, next: NextFunction) {
+//   if (req.headers?.authorization?.startsWith('Bearer ')) {
+//     const idToken = req.headers.authorization.split('Bearer ')[1];
 
-    try {
-      const decodedToken = await auth.verifyIdToken(idToken);
-      req['currentUser'] = decodedToken;
-    } catch (err) {
-      console.log(err);
-    }
-  }
+//     try {
+//       const decodedToken = await auth.verifyIdToken(idToken);
+//       req['currentUser'] = decodedToken;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
 
-  next();
-}
+//   next();
+// }
 
 ///// HELPERS /////
 
