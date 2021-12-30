7102c079c83d6c86f84852e24fddb33beb6a4459
Mason Williams <mwilliams@vantagetechnologies.io>
2021-12-29 20:02:04 -0500

    Added new endpoints

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { lib } from '.';
-import { auth } from './firebase';
 export const app = express();
 
-// Allows cross origin requests
+import { lib } from '.';
+
+// import { auth } from './firebase';
+
 import cors from 'cors';
+
+
+////// MIDDLEWARE  //////
 
 // Allows cross origin requests
 app.use(cors({ origin: true }));
@@ -38,12 +42,13 @@ app.use(
 //   next();
 // }
 
+
 ///// HELPERS /////
 
 /**
  * Validate the stripe webhook secret, then call the handler for the event type
  */
- function runAsync(callback: Function) {
+function runAsync(callback: Function) {
   return (req: Request, res: Response, next: NextFunction) => {
     callback(req, res, next).catch(next);
   };
@@ -52,57 +57,118 @@ app.use(
 
 ///// MAIN API /////
 
-app.get(
-    '/player-service-record', 
-    runAsync(async ({ body }: Request, res: Response) => {
-    
-    const result = await 
-    lib.halo.infinite[process.env.API_VERSION].stats['service-record'].multiplayer({
-        gamertag: body.gamertag,
-        filter: body.filter
-    });
-
-  res.status(200).send( result );
-}));
-
-app.get(
-  '/player-csrs', 
-  runAsync(async ({ body }: Request, res: Response) => {
-  
-  const result = await 
-  lib.halo.infinite[process.env.API_VERSION].stats.csrs({
-    gamertag: body.gamertag,
-    season: body.season
-  });
-
-res.status(200).send( result );
-}));
-
-app.get(
+/**
+ * Get Player Match List
+ */
+ app.get(
   '/match-list', 
   runAsync(async ({ body }: Request, res: Response) => {
   
-  const result = await 
-  lib.halo.infinite[process.env.API_VERSION].stats.matches.list({
-    gamertag: body.gamertag, // required
-    limit: {
-      'count': body.match_count,
-      'offset': body.match_offset
-    },
-    mode: body.match_mode
-  });
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].stats.matches.list({
+      gamertag: body.gamertag, // required
+      limit: {
+        'count': body.match_count, // 1-25
+        'offset': body.match_offset
+      },
+      mode: body.match_mode // matchmade, custom
+    });
 
-res.status(200).send( result );
-}));
+    res.status(200).send( result );
+  }
+));
 
+/**
+ * Get Player Match Details
+ */
 app.get(
-  '/match-info', 
+  '/match-details', 
   runAsync(async ({ body }: Request, res: Response) => {
   
-  const result = await 
-  lib.halo.infinite[process.env.API_VERSION].stats.matches.retrieve({
-    id: body.match_id // required
-  });
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].stats.matches.retrieve({
+      id: body.match_id // required
+    });
 
-res.status(200).send( result );
-}));
+  res.status(200).send( result );
+  }
+));
+
+/**
+ * Get Available Medals
+ */
+ app.get(
+  '/medals', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].metadata.medals.list();
+
+  res.status(200).send( result );
+  }
+));
+
+/**
+ * Get Player Appearance
+ */
+ app.get(
+  '/player-appearance', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+    const result = await lib.halo.infinite['@0.3.3'].appearance({
+      gamertag: body.gamertag
+    });
+
+    res.status(200).send( result );
+  }
+));
+
+/**
+ * Get Player Campaign Service Record
+ */
+ app.get(
+  '/player-campaign-service-record', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].stats['service-record'].campaign({
+        gamertag: body.gamertag,
+    });
+
+    res.status(200).send( result );
+  }
+));
+
+/**
+ * Get Player Multiplayer Service Record
+ */
+app.get(
+    '/player-multiplayer-service-record', 
+    runAsync(async ({ body }: Request, res: Response) => {
+    
+      const result = await 
+      lib.halo.infinite[process.env.API_VERSION].stats['service-record'].multiplayer({
+          gamertag: body.gamertag,
+          filter: body.filter // matchmade:pvp, matchmade:social, matchmade:ranked, matchmade:bots, custom
+      });
+
+    res.status(200).send( result );
+  }
+));
+
+/**
+ * Get Player CSRs
+ */
+ app.get(
+  '/player-csrs', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].stats.csrs({
+      gamertag: body.gamertag,
+      season: body.season // 1
+    });
+
+    res.status(200).send( result );
+  }
+));
