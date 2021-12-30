a5edb95eb51fcd7de7bfc7ba2db2c98ff74de497
Mason Williams <mwilliams@vantagetechnologies.io>
2021-12-29 19:41:08 -0500

    Working API

--- src/api.ts
+++ src/api.ts
@@ -24,33 +24,19 @@ app.use(decodeJWT);
  * Makes the currentUser (firebase) data available on the body.
  */
  async function decodeJWT(req: Request, res: Response, next: NextFunction) {
-    if (req.headers?.authorization?.startsWith('Bearer ')) {
-      const idToken = req.headers.authorization.split('Bearer ')[1];
-  
-      try {
-        const decodedToken = await auth.verifyIdToken(idToken);
-        req['currentUser'] = decodedToken;
-      } catch (err) {
-        console.log(err);
-      }
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+
+    try {
+      const decodedToken = await auth.verifyIdToken(idToken);
+      req['currentUser'] = decodedToken;
+    } catch (err) {
+      console.log(err);
     }
-  
-    next();
   }
 
-///// MAIN API /////
-
-app.get(
-    '/test', 
-    runAsync(async ({ body }: Request, res: Response) => {
-    
-    const result = await lib.halo.infinite[process.env.API_VERSION].stats['service-record'].multiplayer({
-        gamertag: body.gamertag,
-        filter: body.filter
-    });
-
-  res.status(200).send( result );
-}));
+  next();
+}
 
 ///// HELPERS /////
 
@@ -58,7 +44,65 @@ app.get(
  * Validate the stripe webhook secret, then call the handler for the event type
  */
  function runAsync(callback: Function) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      callback(req, res, next).catch(next);
-    };
-  }
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
+
+
+///// MAIN API /////
+
+app.get(
+    '/player-service-record', 
+    runAsync(async ({ body }: Request, res: Response) => {
+    
+    const result = await 
+    lib.halo.infinite[process.env.API_VERSION].stats['service-record'].multiplayer({
+        gamertag: body.gamertag,
+        filter: body.filter
+    });
+
+  res.status(200).send( result );
+}));
+
+app.get(
+  '/player-csrs', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+  const result = await 
+  lib.halo.infinite[process.env.API_VERSION].stats.csrs({
+    gamertag: body.gamertag,
+    season: body.season
+  });
+
+res.status(200).send( result );
+}));
+
+app.get(
+  '/match-list', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+  const result = await 
+  lib.halo.infinite[process.env.API_VERSION].stats.matches.list({
+    gamertag: body.gamertag, // required
+    limit: {
+      'count': body.match_count,
+      'offset': body.match_offset
+    },
+    mode: body.match_mode
+  });
+
+res.status(200).send( result );
+}));
+
+app.get(
+  '/match-info', 
+  runAsync(async ({ body }: Request, res: Response) => {
+  
+  const result = await 
+  lib.halo.infinite[process.env.API_VERSION].stats.matches.retrieve({
+    id: body.match_id // required
+  });
+
+res.status(200).send( result );
+}));
